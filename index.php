035f29da648ae1a59deb4498c8b4c55b8b089b14
Nguyễn Tuấn <anhtuan14072002@gmail.com>
2024-02-25 00:09:02 +0700

    tuấn

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <?php include("box_right/box_right.html")?>
         </div>
         <div class="box_center">
-            
+        <?php include("box_center/box_center.html")?>
         </div>
     </div>
 </body>
