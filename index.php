7eaa2604c4445e8cd127b01c35230fdcb8715c20
NguyenDucTai <vhuy12991@gmail.com>
2024-02-25 00:45:03 +0700

    ntc

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     <title>NCT</title>
     <script src="https://kit.fontawesome.com/50317932e7.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/box_left/left.css">
-    <link rel="stylesheet" href="/box_right/right.css">z
+    <link rel="stylesheet" href="/box_right/right.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <div class="container">
