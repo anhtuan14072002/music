56b4465f786a635ca8815823c7da763134c09cdb
Nguyễn Tuấn <anhtuan14072002@gmail.com>
2024-02-27 19:03:44 +0700

    tuấn

---
+++ mevabenct.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mẹ Và Bé</title>
+    <link rel="stylesheet" href="/box_left/left.css">
+    <link rel="stylesheet" href="/box_center/center.css">
+    <link rel="stylesheet" href="/box_right/right.css">
+</head>
+<body>
+
+    <?php include("box_left/box_left.html")?>
+
+ 
+    <?php include("box_right/box_right.html")?>
+
+    <div class="centernct">
+    <div class="topicnct">
+        <img src="/image/mevabe.jpg" alt="">
+        <div class="topicchild">
+          <p>Theo nhiều nghiên cứu khoa học đã chỉ ra rằng việc cho những bà mẹ đang mang thai và thai nhi nghe nhạc rất tốt cho trí não và sự phát triển của trẻ sau này.
+            <br> NhacCuaTui xin gửi đến những người phụ nữ sắp mang trong mình thiên chức làm mẹ những bản nhạc, những giai điệu du dương, thư giãn và thoải mái nhất.
+            <br> Hãy cùng NhacCuaTui chuẩn bị và chăm sóc tốt nhất cho những thiên thần bé nhỏ sắp chào đời các bạn nhé.
+          </p>
+        </div>
+        <div class="topicmenu">
+          <h2>Mẹ Và Bé</h2>
+          <div class="topicmenuchild">
+            <div class="imgtopic">
+              <img src="/image/cuu.jpg" alt="">
+              <p><a href="#">Sleep Lullaby of Music Box</a></p>
+            </div>
+            <div class="imgtopic">
+              <img src="/image/The Best Relaxing Nighttime.jpg" alt="">
+              <p><a href="#">The Best Baby Lullabies And Re</a></p>
+            </div>
+            <div class="imgtopic">
+              <img src="/image/The Best Lullabies And Sooth.jpg" alt="">
+              <p><a href="#">The Best Lullabies And Soothi</a></p>
+            </div>
+            <div class="imgtopic">
+              <img src="/image/The Best Relaxing Nighttime1.jpg" alt="">
+              <p><a href="#">The Best Relaxing Nighttime</a></p>
+            </div>
+            <div class="imgtopic">
+              <img src="/image/Soothing Lullabies and Relaxi.jpg" alt="">
+              <p><a href="#">Soothing Lullabies and Relaxi</a></p>
+            </div>
+            <div class="imgtopic">
+              <img src="/image/Soothing Nighttime Music Fo.jpg" alt="">
+              <p><a href="#">The Best Lullabies And Sooth</a></p>
+            </div>
+            <div class="imgtopic">
+              <img src="/image/Bedtime Lullabies and Calm.jpg" alt="">
+              <p><a href="#">Soothing Nighttime Music Fo</a></p>
+            </div>
+            <div class="imgtopic">
+              <img src="/image/Bedtime Lullabies and Calmi.jpg" alt="">
+              <p><a href="#">Bedtime Lullabies and Calmi</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+</body>
+</html>
