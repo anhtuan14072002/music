b4993d09382495fbcc5ec47e91dbb439e4b3d1a4
Nguyễn Tuấn <anhtuan14072002@gmail.com>
2024-02-28 14:33:45 +0700

    tuấn

---
+++ vpopthinhhanh.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>V-Pop Thịnh Hành</title>
+    <script src="https://kit.fontawesome.com/50317932e7.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/box_left/left.css">
+    <link rel="stylesheet" href="/box_center/center.css">
+    <link rel="stylesheet" href="/box_right/right.css">
+    <link rel="stylesheet" href="/cssmini/topicmusic.css">
+</head>
+<body>
+    <div class="box_left">
+        <?php include("box_left/box_left.html")?>
+    </div>
+    <div class="box_right">
+        <?php include("box_right/box_right.html")?>
+    </div>
+    <div class="centernct">
+        <div class="topictop">
+            <div class="topicmusic">
+                <img src="/image/vpopth.png" alt="">
+            </div>
+            <div class="topicplaylist">
+                Playlist : <b>V-Pop Thịnh Hành</b>
+                <br>
+                <img src="/image/VA.png" alt="">    <span>V.A</span>
+                <br>
+                <br>
+                <span>15/12/2022</span>
+                <br>
+                <br>
+                <span>V-Pop Thịnh Hành</span>
+                <br>
+                <br>
+                Tags :
+                <button type="button" class="btn btn-secondary">Nhạc Trẻ</button> 
+                <button type="button" class="btn btn-secondary">Cafe</button>
+            </div>
+            </div>
+            <div class="topiclistnct">
+            <h4>Danh sách bài hát</h4>
+            <br>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    <b>TIÊU ĐỀ</b>
+                </div>
+                <div class="musiciannct">
+                    <b>NGHỆ SĨ</b>
+                </div>
+                <div class="numberlisten">
+                    <b>LƯỢT NGHE</b>
+                </div>
+                <div class="timenct">
+                    <b>THỜI GIAN</b>
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Những Lời Hứa Bỏ Quên
+                    <i class="fas fa-crown"></i>
+                </div>
+                <div class="musiciannct">
+                    Vũ, Dear Jane
+                </div>
+                <div class="numberlisten">   
+                    <i class="fas fa-headphones-alt"></i>
+                    3.2M
+                </div>
+                <div class="timenct">
+                    03:56
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Mưa Tháng Sáu
+                </div>
+                <div class="musiciannct">
+                    Văn Mai Hương, GREY D, Trung Quân
+                </div>
+                <div class="numberlisten">
+                    <i class="fas fa-headphones-alt"></i>
+                    3.2M
+                </div>
+                <div class="timenct">
+                    04:17
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Rồi Em Sẽ Gặp Một Chàng Trai Khác
+                    <i class="fas fa-crown"></i>
+                </div>
+                <div class="musiciannct">
+                    The Masked Singer, Hippohappy
+                </div>
+                <div class="numberlisten">
+                    <i class="fas fa-headphones-alt"></i>
+                    3.2M
+                </div>
+                <div class="timenct">
+                    05:04
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Buồn Hay Vui
+                    <i class="fas fa-crown"></i>
+                </div>
+                <div class="musiciannct">
+                    VSOULRPT, MCK, Obito, Ronboogz
+                </div>
+                <div class="numberlisten">
+                    <i class="fas fa-headphones-alt"></i>
+                    2.1M
+                </div>
+                <div class="timenct">
+                    04:51
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Tò Te Tí
+                </div>
+                <div class="musiciannct">
+                    Wren Evans, Itsnk
+                </div>
+                <div class="numberlisten">       
+                    <i class="fas fa-headphones-alt"></i>
+                    58.6K
+                </div>
+                <div class="timenct">
+                    03:15
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Đôi Mình
+                </div>
+                <div class="musiciannct">
+                    Chuột Sấm Sét
+                </div>
+                <div class="numberlisten">
+                    <i class="fas fa-headphones-alt"></i>
+                    281.6K
+                </div>
+                <div class="timenct">
+                    02:57
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Anh Là Ngoại Lệ Của Em
+                    <i class="fas fa-crown"></i>
+                </div>
+                <div class="musiciannct">
+                    Phương Ly
+                </div>
+                <div class="numberlisten">
+                    <i class="fas fa-headphones-alt"></i>
+                    3.5M
+                </div>
+                <div class="timenct">
+                    03:08
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Ngày Mai Người Ta Lấy Chồng
+                    <i class="fas fa-crown"></i>
+                </div>
+                <div class="musiciannct">
+                    The Masked Singer, Voi Bản Đôn
+                </div>
+                <div class="numberlisten">
+                    <i class="fas fa-headphones-alt"></i>
+                    2.3M
+                </div>
+                <div class="timenct">
+                    05:17
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Em Xinh
+                    <i class="fas fa-crown"></i>
+                </div>
+                <div class="musiciannct">
+                    MONO, Onionn
+                </div>
+                <div class="numberlisten">
+                    <i class="fas fa-headphones-alt"></i>
+                    1.6M
+                </div>
+                <div class="timenct">
+                    03:03
+                </div>
+            </div>
+            <div class="titleonct">
+                <div class="listtitlenct">
+                    Nấu ăn cho em
+                    <i class="fas fa-crown"></i>
+                </div>
+                <div class="musiciannct">
+                    Đen, PiaLinh
+                </div>
+                <div class="numberlisten">
+                    <i class="fas fa-headphones-alt"></i>
+                    1.8M
+                </div>
+                <div class="timenct">
+                    04:01
+                </div>
+            </div>
+            </div>
+            
+        </div>
+        
+    
+</body>
+</html>
